ad123be87ec3e5b3e47c4e9bf4700cb191dac25f
SAHIL DAFADE <90718067+Sahil089@users.noreply.github.com>
2024-10-19 14:21:04 +0530

    Update config.py

--- flask_server/config.py
+++ flask_server/config.py
@@ -1,1 +1,6 @@
-
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credintial/cread_new.json"
+client = storage.Client.from_service_account_json('credintial/cread_new.json')
+bucket_name = 'image-website-bucket'
+csv_filename = 'products.csv'
+bucket = client.bucket(bucket_name)
+blob = bucket.blob(csv_filename)
